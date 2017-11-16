b2d951e2fa79857301c28e3d516553d7195a13de
davidkelley <david.james.kelley@gmail.com>
2017-11-16 12:07:39 +0000

    finalizing function

--- functions/transfer/transfer.js
+++ functions/transfer/transfer.js
@@ -8,17 +8,20 @@ import Uploader from './transfer/uploader';
 const client = new CodePipeline({ region: AWS_REGION });
 
 export async function handler(event, context, cb) {
+  const externalExecutionId = context.awsRequestId;
   const pipeline = event['CodePipeline.job'];
   const { id } = pipeline;
   const result = { jobId: id };
   try {
     const urls = await new Uploader(pipeline).perform();
     urls.forEach(url => Logger.info(`Uploaded: ${url}`));
+    result.executionDetails = { externalExecutionId };
     await client.putJobSuccessResult(result).promise();
     cb(null, result);
   } catch (err) {
     Logger.error(err);
     result.failureDetails = {
+      externalExecutionId,
       message: err.message,
       type: 'JobFailed',
     };
